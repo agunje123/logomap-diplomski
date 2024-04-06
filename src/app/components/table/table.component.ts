23b87c3cc026e2cd5ef5f8c406a70f3632f27f74
agunje123 <andrija.gunjevic1@gmail.com>
2024-04-06 23:26:26 +0200

    Add starter table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+
+export interface Kabinet {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
 })
 export class TableComponent {
-
+  displayedColumns: string[] = ['id', 'name', 'actions'];
+  dataSource: Kabinet[] = [
+    { id: 1, name: 'Svasta' },
+    { id: 2, name: 'Nesta' },
+  ];
 }
