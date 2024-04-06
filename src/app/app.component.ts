5afd21c7a5300557b7ba46c2f9d83bfb526c8122
agunje123 <andrija.gunjevic1@gmail.com>
2024-04-06 21:27:25 +0200

    Create new controls component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './components/map/map.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ControlsComponent } from './components/controls/controls.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MapComponent, MatToolbarModule],
+  imports: [RouterOutlet, MapComponent, ControlsComponent, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
