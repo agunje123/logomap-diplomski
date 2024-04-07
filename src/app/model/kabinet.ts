017eaf78fbb70c7cede2e89ed11a0669372e5a4c
agunje123 <andrija.gunjevic1@gmail.com>
2024-04-07 21:04:56 +0200

    Create kabinet model

---
+++ src/app/model/kabinet.ts
@@ -0,0 +1,10 @@
+export interface Kabinet {
+    id?: number,
+    name?: string,
+    address?: string,
+    website?: string,
+    phoneNumber?: string,
+    longitude?: number,
+    latitude?: number,
+    geometry?: string
+}
