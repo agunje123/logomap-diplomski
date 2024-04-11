c7a70f4e2eb0957d2f390f26adc986cd7eda812b
agunje123 <andrija.gunjevic1@gmail.com>
2024-04-11 22:46:28 +0200

    Create supabase service functions

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../environment';
+import { Kabinet } from '../model/kabinet';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SupabaseService {
-  createKabinet() {}
+  protected supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
+
+  createKabinet(kabinetList: Kabinet[]) {
+    kabinetList.forEach((kabinet) => {
+      this.supabase.from('kabineti').insert({
+        name: kabinet.name,
+        address: kabinet.address,
+        website: kabinet.website,
+        phone_number: kabinet.phoneNumber,
+        geometry: `POINT(${kabinet.longitude} ${kabinet.latitude})`,
+      });
+    });
+  }
+
 }
