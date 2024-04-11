20aad4a4d8d888c0463025380ce9c811ab884065
agunje123 <andrija.gunjevic1@gmail.com>
2024-04-11 13:55:13 +0200

    Parse csv

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-
-export interface Kabinet {
-  id: number;
-  name: string;
-}
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Kabinet } from '../../model/kabinet';
 
 @Component({
   selector: 'app-table',
@@ -16,11 +12,64 @@ export interface Kabinet {
   styleUrl: './table.component.scss',
 })
 export class TableComponent {
+  dataSource = new MatTableDataSource<Kabinet>([]);
   displayedColumns: string[] = ['id', 'name', 'actions'];
-  dataSource: Kabinet[] = [
-    { id: 1, name: 'Svasta' },
-    { id: 2, name: 'Nesta' },
-  ];
+  data: Kabinet[] = [];
 
-  onFileSelected(e: Event) {}
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.parseCsv(file);
+    }
+  }
+
+  parseCsv(file: File) {
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+      const csv = reader.result as string;
+      const rows = csv.split('\n');
+      for (let i = 1; i < rows.length; i++) {
+        const row = rows[i];
+        const columns = this.parseCsvRow(row);
+        const kabinet: Kabinet = {
+          id: parseInt(columns[0]) + 1,
+          name: columns[1],
+          address: columns[2],
+          website: columns[3],
+          phoneNumber: columns[4],
+          longitude: parseFloat(columns[5]),
+          latitude: parseFloat(columns[6]),
+        };
+        if (kabinet.id) {
+          this.data.push(kabinet);
+        }
+      }
+      this.dataSource.data = this.data;
+      console.log(this.dataSource.data);
+    };
+    reader.onerror = () => {
+      console.error('Error reading CSV file');
+    };
+  }
+
+  parseCsvRow(row: string): string[] {
+    const columns = [];
+    let currentColumn = '';
+    let withinQuotes = false;
+
+    for (let i = 0; i < row.length; i++) {
+      const char = row[i];
+      if (char === ',' && !withinQuotes) {
+        columns.push(currentColumn.trim());
+        currentColumn = '';
+      } else if (char === '"' && (i === 0 || row[i - 1] !== '\\')) {
+        withinQuotes = !withinQuotes;
+      } else {
+        currentColumn += char;
+      }
+    }
+    columns.push(currentColumn.trim());
+    return columns;
+  }
 }
